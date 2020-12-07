b4348d8aacc0d197e2472e9df0633497a345c702
Jiashuo Li <jiasli@microsoft.com>
2020-12-07 14:39:19 +0800

    Add comments

--- color-demo.py
+++ color-demo.py
@@ -86,10 +86,12 @@ def print_rgb(text, rgb):
 def main():
     enable_vt_mode()
 
-    # Print some emoji
+    # 🎃👻🦉🧙‍🔮: some emoji
     # ✓: https://www.compart.com/en/unicode/U+2713
     # ⦿: https://www.compart.com/en/unicode/U+29BF
     # ⦾: https://www.compart.com/en/unicode/U+29BE
+    # 测试: "test" in Chinese
+    # テスト: "test" in Japanese
     print("🎃👻🦉🧙‍🔮✓⦿⦾测试テスト")
 
     # Print text in the basic 16 colors
