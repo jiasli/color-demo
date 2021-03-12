3eb42629aea84b4f4eb942030d348d89a29066bd
Jiashuo Li <jiasli@microsoft.com>
2021-03-12 13:50:00 +0800

    Add more modes

--- color-demo.py
+++ color-demo.py
@@ -1,7 +1,6 @@
 import colorsys
 import os
-
-import numpy
+import sys
 
 DEFAULT = '\x1b[0m'
 
@@ -40,56 +39,106 @@ def print_color_index(text, color_index):
     print_with_esc(text, '\x1b[38;5;{}m'.format(color_index))
 
 
+def print_block(rgb):
+    print_rgb("█", rgb)
+
+
 def main():
-    if os.name == 'nt':
-        from win_vt import enable_vt_mode
-        enable_vt_mode()
+    if 'vt' in sys.argv:
+        if os.name == 'nt':
+            from win_vt import enable_vt_mode
+            enable_vt_mode()
 
-    # 🎃👻🦉🧙‍🔮: some emoji
-    # ✓: https://www.compart.com/en/unicode/U+2713
-    # ⦿: https://www.compart.com/en/unicode/U+29BF
-    # ⦾: https://www.compart.com/en/unicode/U+29BE
-    # 测试: "test" in Chinese
-    # テスト: "test" in Japanese
-    print("Test Unicode: 🎃👻🦉🧙‍🔮✓⦿⦾测试テスト")
+    if 'text' in sys.argv:
+        # Print text in the 16 basic colors
 
-    # Print text in the basic 16 colors
-    for color in COLORS:
-        text = "This is some text in {:14s} {:8s}  █████████████████".format(
-            color, COLORS[color].replace('\x1b', '\\x1b'))
-        colored_text = COLORS[color] + text + DEFAULT
-        print("{:14s}: {}".format(color, colored_text))
+        # 🎃👻🦉🧙‍🔮: some emoji
+        # ✓: https://www.compart.com/en/unicode/U+2713
+        # ⦿: https://www.compart.com/en/unicode/U+29BF
+        # ⦾: https://www.compart.com/en/unicode/U+29BE
+        # 测试: "test" in Chinese
+        # テスト: "test" in Japanese
+        print("Test Unicode: 🎃👻🦉🧙‍🔮✓⦿⦾测试テスト")
 
-    print()
+        for color in COLORS:
+            text = "This is some text in {:14s} {:8s}  █████████████████".format(
+                color, COLORS[color].replace('\x1b', '\\x1b'))
+            colored_text = COLORS[color] + text + DEFAULT
+            print("{:14s}: {}".format(color, colored_text))
 
-    # Print text in the 256 color table
-    base = 16
-    print("  0~ 15 ", end='')
-    for i in range(base):
-        print_color_index("█", i)
-    print()
-    row = 6
-    column = 36
-    for i in range(0, row):
-        print(f"{base + i * column:3d}~{base + i * column + column - 1:3d} ", end='')
-        for j in range(0, column):
-            print_color_index("█", base + i * column + j)
+    if 'table' in sys.argv:
+        # Print text in the 256 color table
+        base = 16
+        print("  0~ 15 ", end='')
+        for i in range(base):
+            print_color_index("█", i)
         print()
-    print("232~255 ", end='')
-    for i in range(232, 256):
-        print_color_index("█", i)
-    print()
-    print()
-
-    # Draw an HSV map
-    v = 1
-    for s in numpy.arange(1, 0, -0.05):
-        for h in numpy.arange(0, 1, 0.01):
-            rgb = tuple(int(255 * x) for x in colorsys.hsv_to_rgb(h, s, v))
-            print_rgb("█", rgb)
-            # print(rgb)
+        row = 6
+        column = 36
+        for i in range(0, row):
+            start = base + i * column
+            end = base + i * column + column - 1
+            print(f"{start:3d}~{end:3d} ", end='')
+            for j in range(0, column):
+                print_color_index("█", start + j)
+            print()
+        print("232~255 ", end='')
+        for i in range(232, 256):
+            print_color_index("█", i)
         print()
+
+    if 'hsv' in sys.argv:
+        # Draw HSV map dominated by h
+        for h in range(11):
+            for v in range(21):
+                for s in range(51):
+                    rgb = tuple(int(255 * x) for x in colorsys.hsv_to_rgb(h/10, s/50, 1-v/20))
+                    print_block(rgb)
+                print()
+
+    if 'hsv-v' in sys.argv:
+        # Draw HSV map dominated by v
+        for v in range(11):
+            for s in range(21):
+                for h in range(101):
+                    rgb = tuple(int(255 * x) for x in colorsys.hsv_to_rgb(1-h/100, 1-s/20, v/10))
+                    print_block(rgb)
+                print()
+
+    if 'hsv-v-full' in sys.argv:
+        # Draw HSV map with full v
+        v = 1
+        for s in range(21):
+            for h in range(101):
+                rgb = tuple(int(255 * x) for x in colorsys.hsv_to_rgb(1-h/100, 1-s/20, v))
+                print_block(rgb)
+            print()
+
+    if 'rgb' in sys.argv:
+        # Draw RGB map
+        def _iter_to_ff(step):
+            for value in range(0, 0xff, step):
+                yield value
+            yield 0xff
+
+        for r in _iter_to_ff(0x10):
+            # Print r label
+            print(f'\x1b[38;2;255;0;0m{r:02x} ', end='')
+
+            # Print b label
+            b_label = ['{:4s}'.format('{:02x}'.format(b)) for b in _iter_to_ff(0x10)]
+            print('\x1b[38;2;0;0;255m' + ''.join(b_label))
+
+            for g in _iter_to_ff(0x10):
+                # Print g label
+                print(f'\x1b[38;2;0;255;0m{g:02x} ', end='')
+                for b in _iter_to_ff(0x4):
+                    print_block((r, g, b))
+                print()
+            print()
 
 
 if __name__ == '__main__':
+    if len(sys.argv) == 1:
+        sys.argv.extend(['text', 'hsv-v-full'])
     main()
